0cb4ed36eaf80a70a1a1fde10c4a102812cec690
Gosha94 <gerizch@rambler.ru>
2021-03-01 16:47:51 +0300

    Gosha-Add New Styles

---
+++ ASix_Training.Wpf.CustomWindowStyle/ViewModel/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using PropertyChanged;
+using System.ComponentModel;
+
+namespace ASix_Training.Wpf.CustomWindowStyle.ViewModel
+{
+    /// <summary>
+    /// Базовая модель представления которая поддерживает события PropertyChanged, все дочерние классы будут пробрасывать свои свойства во вью без дублей кода
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Событие, поднимающееся, когда свойство дочернего клсса меняет значение
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public void OnPropertyChanged(string name)
+        {
+            //PropertyChanged();
+        }
+
+    }
+}
---
+++ ASix_Training.Wpf.CustomWindowStyle/ViewModel/RelayCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ASix_Training.Wpf.CustomWindowStyle.ViewModel
+{
+    /// <summary>
+    /// Базовая команда, которая запускает Action
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        #region Private Members
+
+        private Action _action;
+
+        #endregion
+
+        #region Public Events
+
+        /// <summary>
+        /// Событие запускается, когда значение <see cref="CanExecute(object)"/> меняется
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        /// <param name="action"></param>
+        public RelayCommand ( Action action )
+        {
+            this._action = action;
+        }
+        #endregion
+
+        /// <summary>
+        /// Команда, которая всегда выполняется
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        // Выполняет метод действия из команды
+        public void Execute(object parameter)
+        {
+            this._action();
+        }
+    }
+}
