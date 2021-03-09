f6d06b062245ffbdb66ba2a318b9c03c00db9527
Gosha94 <gerizch@rambler.ru>
2021-03-09 15:10:22 +0300

    Update WindowViewModel.cs

--- ASix_Training.Wpf.CustomWindowStyle/ViewModel/WindowViewModel.cs
+++ ASix_Training.Wpf.CustomWindowStyle/ViewModel/WindowViewModel.cs
@@ -34,16 +34,39 @@ namespace ASix_Training.Wpf.CustomWindowStyle.ViewModel
         public int ResizeBorder { get; set; } = 6;
 
         public Thickness ResizeBorderThickness { get => new Thickness(this.ResizeBorder); }
-
+        
+        /// <summary>
+        /// Отступ вокруг окна для создания тени
+        /// </summary>
         public int OuterMarginSize
         {
             // Если окно развернуто на весь экран, то мы ставим ему отступ в 0, иначе отступ есть
             get => _window.WindowState == WindowState.Maximized ? 0 : _outerMarginSize;
             set
             {
-                //_outerMarginSize = 
+                _outerMarginSize = value;
             }
         }
+        /// <summary>
+        /// Отступ вокруг окна для создания тени  в представлении
+        /// </summary>
+        public Thickness OuterMarginSizeThickness { get => new Thickness(this.OuterMarginSize); }
+
+        /// <summary>
+        /// Радиус скругления углов окна
+        /// </summary>
+        public int WindowsRadius
+        {            
+            get => _window.WindowState == WindowState.Maximized ? 0 : _windowRadius;
+            set
+            {
+                _windowRadius = value;
+            }
+        }
+        /// <summary>
+        /// Радиус скругления углов окна для представления
+        /// </summary>
+        public CornerRadius WindowCornerRadius { get => new CornerRadius(this.WindowsRadius); }
 
         #endregion
 
@@ -56,6 +79,11 @@ namespace ASix_Training.Wpf.CustomWindowStyle.ViewModel
         public WindowViewModel( Window window)
         {
             this._window = window;
+
+            _window.StateChanged += (sender, e) => 
+            {
+
+            };
         }
 
         #endregion
