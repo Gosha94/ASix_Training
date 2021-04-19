6e2a50cd5c8db654bd8a32f9f84fb6ac4f953d7b
Gosha94 <gerizch@rambler.ru>
2021-04-19 23:39:36 +0300

    Gosha Add Page animation

---
+++ ASix_Training.Wpf.CustomWindowStyle/Animation/PageAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASix_Training.Wpf.CustomWindowStyle.Animation
+{
+    /// <summary>
+    /// Стили анимации страниц для отображения/скрытия
+    /// </summary>
+    public enum PageAnimation
+    {
+        None = 0,
+
+        SlideAndFadeInFromRight = 1,
+
+        SlideAndFadeOutToLeft = 2,
+    }
+}
---
+++ ASix_Training.Wpf.CustomWindowStyle/Pages/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using ASix_Training.Wpf.CustomWindowStyle.Animation;
+using System.Windows.Controls;
+
+namespace ASix_Training.Wpf.CustomWindowStyle.Pages
+{
+    /// <summary>
+    /// Базовая страница для всех страниц проекта, для расширения базовой функциональности
+    /// </summary>
+    public class BasePage : Page
+    {
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
+    }
+}
